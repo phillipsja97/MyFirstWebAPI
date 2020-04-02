e83da48b2f70b291b15e689c48d9127db8a42dbf
Jamie <phillipsja97@gmail.com>
2020-04-01 21:11:09 -0500

    Add and Update have been finished.

--- MyFirstWebAPI/Models/Animal.cs
+++ MyFirstWebAPI/Models/Animal.cs
@@ -7,6 +7,7 @@ namespace MyFirstWebAPI.Models
 {
     public class Animal
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public int Weight { get; set; }
